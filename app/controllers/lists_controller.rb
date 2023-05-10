06adffcc5d1c4aff0e23a40b976b26eeeff26e14
KeiMonden <129693543+kei9625@users.noreply.github.com>
2023-05-10 09:30:37 +0000

    [finish]アプリケーションを完成させよう - ch7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -33,9 +33,10 @@ class ListsController < ApplicationController
   end
 
   def destroy
-    list = List.find(params[:id]) # データ（レコード）を1件取得
-    list.destroy # データ（レコード）を削除
-    redirect_to '/lists' # 投稿一覧画面へリダイレクト
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト
+  end
 
   private
   # ストロングパラメータ
