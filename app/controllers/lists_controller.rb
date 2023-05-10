4ed03751f3d64d45061df7ca7a171292b7f91040
KeiMonden <129693543+kei9625@users.noreply.github.com>
2023-05-10 09:03:37 +0000

    [wip]アプリケーションを完成させよう - ch7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,8 +9,9 @@ class ListsController < ApplicationController
     list = List.new(list_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to top_path
+    # redirect_to '/top' を削除して、以下コードに変更
+    # 詳細画面へリダイレクト
+    redirect_to list_path(list.id)
   end
 
   def index
@@ -18,14 +19,27 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
   end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id]) # データ（レコード）を1件取得
+    list.destroy # データ（レコード）を削除
+    redirect_to '/lists' # 投稿一覧画面へリダイレクト
 
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
