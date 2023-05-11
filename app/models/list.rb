fa538b3354b331d754026d1e6bc2277f65f4a5c8
KeiMonden <129693543+kei9625@users.noreply.github.com>
2023-05-11 06:39:34 +0000

    [finish] アプリケーションを完成させよう - ch9

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,9 @@
 class List < ApplicationRecord
   has_one_attached :image
+  
+  validates :title, presence: true
+  validates :body, presence: true
+  
+  #10章で一時的にコメントアウト
+  #validates :image, presence: true
 end
